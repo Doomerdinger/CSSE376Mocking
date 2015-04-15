9882574f5d5b15fe3f24deca81c66d2f92778a29
Alec Tiefenthal <alectief@gmail.com>
2015-04-15 15:28:42 -0400

    Revert "Revert "Done""
    This reverts commit 71bf2eda2b78a90ffc3f6139fc65d91533ef0602.

--- ExpediaTest/CarTest.cs
+++ ExpediaTest/CarTest.cs
@@ -43,6 +43,49 @@ namespace ExpediaTest
 			var target = new Car(7);
 			Assert.AreEqual(10*7*.8, target.getBasePrice());
 		}
+
+        [TestMethod()]
+        public void TestThatCarGetsLocationFromDatabase()
+        {
+            IDatabase mockDB = mocks.StrictMock<IDatabase>();
+            String carLocation1 = "Bottom of the ocean";
+            String carLocation2 = "Germany";
+            Expect.Call(mockDB.getCarLocation(1)).Return(carLocation1);
+            Expect.Call(mockDB.getCarLocation(2)).Return(carLocation2);
+            mocks.ReplayAll();
+            Car target = new Car(10);
+            target.Database = mockDB;            String result;
+            result = target.getCarLocation(1);
+            Assert.AreEqual(carLocation1, result);
+
+            result = target.getCarLocation(2);
+            Assert.AreEqual(carLocation2, result);
+
+            mocks.VerifyAll();
+        }
+
+        [TestMethod()]
+        public void TestThatCarGetsRightMileage()
+        {
+            IDatabase mockDatabase = mocks.StrictMock<IDatabase>();
+            Int32 miles = 96324;
+            Expect.Call(mockDatabase.Miles).PropertyBehavior();
+            mocks.ReplayAll();
+            mockDatabase.Miles = miles;
+            var target = new Car(10);
+            target.Database = mockDatabase;
+            Int32 mileNum = target.Mileage;
+            Assert.AreEqual(mileNum, miles);
+            mocks.VerifyAll();
+        }
+
+        [TestMethod()]
+        public void TestThatObjectMotherBMWRightName()
+        {
+            String name = "BMW red car thing";
+            var target = ObjectMother.BMW();
+            Assert.AreEqual(name, target.Name);
+        }
 		
 		[TestMethod]
 		[ExpectedException(typeof(ArgumentOutOfRangeException))]
